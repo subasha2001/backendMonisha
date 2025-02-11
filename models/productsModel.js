0043cb633f9eaa865fec76d8bebb4994988771a6
Subash <subashayyanar1@gmail.com>
2025-02-11 12:33:25 +0530

    productsModel

--- models/productsModel.js
+++ models/productsModel.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const productSchema = new Schema({
-  name: { type: String },
-  category: { type: String },
-  subCategory: { type: String },
-  description: { type: String },
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  subCategory: { type: String, required: true },
+  description: { type: String, required: true },
   longDescription: { type: String },
-  image_url: [{ type: String }],
+  images: { type: String, required: true },
   stock: { type: Number },
-  brand: { type: String },
+  brand: { type: String, required: true },
   price: { type: String },
-  part_number: {type: String}
+  part_number: { type: String }
 }, { timestamps: true });
 
 const ProductsModel = model("products", productSchema);
